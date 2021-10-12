044e1b0a2514679dd8ff821cf8c1dd1477a46f53
Diego Gonzalez <51039803+DiegoJGonzalez23@users.noreply.github.com>
2021-10-12 09:45:50 -0400

    Add files via upload

---
+++ dancemooves.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class dancemooves {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int amountcows = Integer.parseInt(st.nextToken());
+		int changes = Integer.parseInt(st.nextToken());
+		
+		
+		
+		for(int i = 0; i < changes; i++){
+			
+			st = new StringTokenizer(br.readLine());
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+
+	}
+	
+	
+	
+	public static class cow{
+		public int cowid;
+		public HashSet<Integer> visited;
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
